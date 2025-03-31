b5fc8e0ddf7bfb997fa7db3860fd6def620d2b53
soraya-skavinski-thaddeus <sorayaskavinski@thaddeus.org>
2025-03-31 17:33:38 -0300

    installed body-parser, cors, passport, passport-github2

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 require ('dotenv').config();
 
-const session = require ('express-session')
+const express = require ('express');
+const bodyParser = require ('body-parser');
+const session = require ('express-session');
+const mongodb = require('./data/database');
+const cors = require ('cors');
+const passport = require ('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
+
+const port = process.env.PORT || 3001;
+const app = express();
+app.use(bodyParser.json());
+
