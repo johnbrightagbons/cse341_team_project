0eed3060744715b8e5cab42b07403928aa804544
soraya-skavinski-thaddeus <sorayaskavinski@thaddeus.org>
2025-04-11 17:23:29 -0300

    ..

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5002;
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
