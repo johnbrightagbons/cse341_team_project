491155b59262d25b07214bcf212e92928785b1f3
soraya-skavinski-thaddeus <sorayaskavinski@thaddeus.org>
2025-04-12 18:10:20 -0300

    changed requirement for image

--- models/product.model.js
+++ models/product.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const productSchema = new  mongoose.Schema({
     name:{type: String, required:true},
     description: {type:String, required:true},
-    image:{type:String},
+    image:{type:String, required: false},
     price:{type:Number, required:true}
 },{timestamps:true})
 
