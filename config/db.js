4253b9333af62e49c560de7340b4aeddda2b3516
soraya-skavinski-thaddeus <sorayaskavinski@thaddeus.org>
2025-04-12 13:19:28 -0300

    corrected .env

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async()=>{
     try {
-     await mongoose.connect(process.env.MONGO_URL)
+     await mongoose.connect(process.env.MONGO_URI)
         console.log("Connected to MongoDB Atlas");
     } catch (error) {
         console.error("Failed to connect:", error);
