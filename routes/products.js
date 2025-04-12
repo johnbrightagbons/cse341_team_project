6ff8552ade008651bfe9aa270a558d926388c928
soraya-skavinski-thaddeus <sorayaskavinski@thaddeus.org>
2025-04-12 16:38:22 -0300

    image upload corrected

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,7 @@
 import express from "express"
 import { getAll, getSingle, createProduct, updateProduct, deleteProduct } from "../controllers/productController.js";
 import { validateProduct } from "../utils/validateProduct.js"; 
-import {upload} from "../middleware/imageUpload.js";
+import upload from "../middleware/imageUpload.js";
 import { isAuthenticated} from "../middleware/authMiddleware.js" ;
 const  router = express.Router();
 
