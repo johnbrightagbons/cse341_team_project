a2478b6a643d7f61bf778d35c4bdce55c73cd48b
Edward Kamwi <157596090+EddyKamwi@users.noreply.github.com>
2025-04-14 15:37:35 +0200

    improving the authentication

--- routes/index.js
+++ routes/index.js
@@ -43,20 +43,20 @@ routes.get("/dashboard", isAuth, (req, res) => {
 // CREATE
 routes.post("/users", validateUser, UserController.create);
 routes.post("/orders", validateOrder, OrderController.create);
-routes.post("/payments", isAuth, validatePayment, PaymentController.create);
-routes.post("/products", isAuth, validateProduct, ProductController.create);
+routes.post("/payments", validatePayment, PaymentController.create);
+routes.post("/products", validateProduct, ProductController.create);
 
 // READ Many
 routes.get("/users", UserController.index);
 routes.get("/orders", OrderController.index);
 routes.get("/payments", PaymentController.index);
-routes.get("/products", ProductController.index);
+routes.get("/products", isAuth, ProductController.index);
 
 // READ ONE
 routes.get("/users/:id", UserController.show);
 routes.get("/orders/:id", OrderController.show);
 routes.get("/payments/:id", PaymentController.show);
-routes.get("/products/:id", ProductController.show);
+routes.get("/products/:id", isAuth, ProductController.show);
 
 // UPDATE
 routes.put("/users/:id", validateUpdate, UserController.update);
